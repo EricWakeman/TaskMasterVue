1f53727b1eb25a5d15ba09b01d3bc11ea7e126d3
Eric Wakeman <erwakeman@gmail.com>
2021-08-04 10:36:50 -0600

    push

--- TaskMasterVue.client/src/components/CreateList.vue
+++ TaskMasterVue.client/src/components/CreateList.vue
@@ -11,7 +11,7 @@
       <form class="modal-content" @submit.prevent="createList" id="list-form">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLongTitle">
-            Modal title
+            New List
           </h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
